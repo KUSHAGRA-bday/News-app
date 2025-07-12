c94d34c59212935b844820ed33a4330f6dc835e6
KUSHAGRA-bday <srivastavakushagra587@gmail.com>
2025-07-12 21:02:01 +0530

    .

--- api/news.js
+++ api/news.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     const { query } = req.query;
     const apiKey = "da0722985fa54f1a885be4817a174d3b";
-    const url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(query)}&sortBy=popularity&pageSize=10&apiKey=${apiKey}`;
+    const url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(query)}&sortBy=publishedAt&pageSize=25&apiKey=${apiKey}`;
 
     try {
         const response = await fetch(url);
